6518038c9bc2da0adea5f839b80662fbd9c107c7
Wei Hien <weihien@supplycart.my>
2020-05-24 13:10:26 +0800

    Update Homepage

--- modules/home.R
+++ modules/home.R
@@ -2,14 +2,56 @@ homeUI <- function(id) {
     ns <- NS(id)
     
     div(
-        class="jumbotron",
+        class = "jumbotron",
         fluidPage(
             tags$head(
+                tags$link(rel = "stylesheet", type = "text/css", href = "css/styles.css"),
                 tags$link(rel = "stylesheet", type = "text/css", href = "css/home.css")
             ),
 
-            h1(class="text-center", "CoronaJob"),
-            h3(class="text-center", "An analysis on economic impact of COVID-19 towards Asia region")
+            h1(class = "text-center", "CoronaJob"),
+            h3(class = "text-center", "An analysis on economic impact of COVID-19 towards Malaysia and neighouring countries"),
+
+            div(class = "row mt-16",
+                div(class = "col-md-4",
+                    div(class = "panel panel-primary",
+                        div(class = "panel-heading",
+                            h2(
+                                class = "panel-title", 
+                                icon("line-chart", "fa-2x"),
+                                "Confirmed Cases"
+                            )
+                        ),
+                        div(class="panel-body", "1.234k")
+                    )
+                ),
+
+                div(class = "col-md-4",
+                    div(class = "panel panel-primary",
+                        div(class = "panel-heading",
+                            h3(
+                                class = "panel-title", 
+                                icon("line-chart", "fa-2x"),
+                                "Recovered Cases"
+                            )
+                        ),
+                        div(class="panel-body", "1.234k")
+                    )
+                ),
+                
+                div(class = "col-md-4",
+                    div(class = "panel panel-primary",
+                        div(class = "panel-heading",
+                            h3(
+                                class = "panel-title", 
+                                icon("line-chart", "fa-2x"),
+                                "Hospitalized"
+                            )
+                        ),
+                        div(class="panel-body", "1.234k")
+                    ),
+                ),
+            )
         )
     )
 }
