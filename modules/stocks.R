181010b841bf06a04cf1c286072df212d8d3f071
Wei Hien <weihien@supplycart.my>
2020-06-03 11:32:11 +0800

    Add KLSE Stock chart with period slider

--- modules/stocks.R
+++ modules/stocks.R
@@ -1,49 +1,57 @@
 library(quantmod)
+library(shinyWidgets)
 
 stocksUI <- function(id) {
     ns <- NS(id)
     
     fluidPage(
-        titlePanel("Stocks"),
-
-        sidebarLayout(
-            sidebarPanel(
-                selectInput(ns("from"), 
-                    label = "Choose Period", 
-                    choices = c(
-                        "This year" = "2020-01-01",
-                        "Since MCO" = "2020-03-18"
-                    ),
-                    selected = "2020-01-01"
-                ),
-
-                selectInput(ns("group"), 
-                    label = "Show by", 
-                    choices = c(
-                        "Daily" = "daily",
-                        "Weekly" = "weekly",
-                        "Monthly" = "monthly"
-                    ),
-                    selected = "Daily"
-                ),
+        chooseSliderSkin("Flat"),
+        sliderTextInput(
+            ns("period"),
+            label = NULL,
+            choices = c(
+                "1 year before MCO",
+                "6 months before MCO",
+                "3 months before MCO",
+                "MCO Starts",
+                "Now"
             ),
-
-            mainPanel(
-                plotOutput(ns("klse")),
-                br(),
-                plotOutput(ns("klse2")),
-            ),
+            selected = c("3 months before MCO", "Now"),
+            width = "100%"
         ),
+
+        h2(textOutput(ns("period"))),
+
+        selectInput(ns("group"), 
+            label = "Show by", 
+            choices = c(
+                "Daily" = "daily",
+                "Weekly" = "weekly",
+                "Monthly" = "monthly"
+            ),
+            selected = "Daily"
+        ),
+
+        plotOutput(ns("klse"))
     )
 }
 
 stocks <- function(input, output, session) {
     klseStock <- reactive({
+        periodMaps <- c(
+            "1 year before MCO" = "2019-03-18",
+            "6 months before MCO" = "2019-09-18",
+            "3 months before MCO" = "2019-12-18",
+            "MCO Starts" = "2020-03-18",
+            "Now" = as.character(Sys.Date()),
+            grid = TRUE
+        )
+
         getSymbols(
-            "^klse", 
+            "^KLSE", 
             src = "yahoo",
-            from = input$from,
-            to = Sys.Date(),
+            from = periodMaps[input$period[1]],
+            to = periodMaps[input$period[2]],
             auto.assign = FALSE
         )
     })
@@ -66,5 +74,9 @@ stocks <- function(input, output, session) {
         if (input$group == "daily") {
             addBBands(); addVo(); addMACD();
         }
+    })
+
+    output$period <- renderText({
+        paste("KLSE Index : ", input$period[1], " - ", input$period[2])
     })
 }
