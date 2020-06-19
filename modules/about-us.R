e097ac821c1145ed2e2d8fa43c96312de9160b5c
Wei Hien <weihien@supplycart.my>
2020-06-19 23:34:31 +0800

    Add student ID

--- modules/about-us.R
+++ modules/about-us.R
@@ -11,22 +11,26 @@ aboutUsUI <- function(id) {
             div(class = "col-md-3",
                 img(class = "img-responsive", src = "images/team-member-1.jpg"),
                 br(),
-                h4(class = "text-center", "Anders Ng")
+                h4(class = "text-center", "Ng Keat Zhao"),
+                h4(class = "text-center", "(A17217743)"),
             ),
             div(class = "col-md-3",
                 img(class = "img-responsive", src = "images/team-member-2.jpg"),
                 br(),
-                h4(class = "text-center", "Ang Lin Siang")
+                h4(class = "text-center", "Ang Lin Siang"),
+                h4(class = "text-center", "(A17174507)"),
             ),
             div(class = "col-md-3",
                 img(class = "img-responsive", src = "images/team-member-3.jpg"),
                 br(),
-                h4(class = "text-center", "Cheong Wei Hien")
+                h4(class = "text-center", "Cheong Wei Hien"),
+                h4(class = "text-center", "(A17218461)"),
             ),
             div(class = "col-md-3",
                 img(class = "img-responsive", src = "images/team-member-4.jpg"),
                 br(),
-                h4(class = "text-center", "Lai Hoi Yee")
+                h4(class = "text-center", "Lai Hoi Yee"),
+                h4(class = "text-center", "(A17218659)"),
             ),
         )
     )
